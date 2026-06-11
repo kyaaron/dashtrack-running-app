3c53fc2f3d1e85497245334cf96b560d21779339
aaronmccollum <mccollumaarons@gmail.com>
2026-06-11 14:38:23 +0900

    removed unnecessary CSS code and assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import WorkoutForm from './components/WorkoutForm'
+import WorkoutStats from './components/WorkoutStats'
+import WorkoutTable from './components/WorkoutTable'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hello World</h1>
+      <WorkoutForm />
+      <WorkoutStats />
+      <WorkoutTable />
     </>
   )
 }
